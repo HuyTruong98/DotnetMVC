25f1ac648e76b1fb75d6d070883cb0ae3217587c
HuyTruong98 <truonghoanghuy98@gmail.com>
2025-09-20 21:00:09 +0700

    update list seen

--- obj/Debug/net9.0/OnlineStoreMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/OnlineStoreMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b1a93c4a647985704c1be1b7624ecce7eecdd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+374e59b64eebb11ca845037f4179a4b9a26a59af")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
